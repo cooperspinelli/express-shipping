db4c6901219468232a7ae1cc2d142eff635233c5
Ben Briones <benbriones50@gmail.com>
2024-03-26 16:52:32 -0700

    finished tests

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,28 @@
 "use strict";
 
+const { SHIPIT_API_KEY, SHIPIT_SHIP_URL } = require("./shipItApi")
+const fetchMock = require("fetch-mock");
+
+
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+  fetchMock.post(SHIPIT_SHIP_URL, {
+   body: {
+    "receipt": {
+      "itemId": 1234,
+      "name": "started ",
+      "addr": "1234 street",
+      "zip": "1234",
+      "shipId": 7875
+   }},
+   status: 201
+  })
+
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +30,6 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+
+  expect(shipId).toEqual(7875);
 });
